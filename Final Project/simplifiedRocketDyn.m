c1b50bd51fce5055ef29bbc57011ba97cfd7f746
Ed <eizaguir@berkeley.edu>
2019-12-12 18:09:20 -0800

    Fixed mistake in Rocket Dyn and Attempt at Non-Constant Force

--- Final Project/simplifiedRocketDyn.m
+++ Final Project/simplifiedRocketDyn.m
@@ -1,4 +1,4 @@
-function znext = Rocketdyn(z,u)
+function znext = simplifiedRocketDyn(z,u)
 TS = 0.1;
 m = 27648;
 l = 70;
@@ -11,8 +11,8 @@ b = 9;
 Fd1 = 1/2*rho*z(4)^2*Cd*A_tot*sin(u(3));
 Fd2 = 1/2*rho*z(4)^2*Cd*A_tot*sin(u(4));
 
-znext(1,1) = z(1) + TS*z(2);
-znext(2,1) = z(2) - TS*(l/2/J*u(2) + (Fd1-Fd2)*b/2/J);
+znext(1,1) = z(1);
+znext(2,1) = z(2);
 znext(3,1) = z(3) + TS*z(4);
-znext(4,1) = z(4) + TS*(g - (1/m)*u(1) - 1/m*(Fd1+Fd2));
+znext(4,1) = z(4) + TS*(g - (1/m)*u(1));
 end
