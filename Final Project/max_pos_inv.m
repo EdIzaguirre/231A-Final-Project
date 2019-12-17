706569e26bcb55b4c19af0604ea72d236772466b
shakshihimmatramka <shakshihimmatramka@github.com>
2019-12-18 03:49:16 +0800

    Update max_pos_inv.m

--- Final Project/max_pos_inv.m
+++ Final Project/max_pos_inv.m
@@ -1,13 +1,13 @@
 function [Oinf,converged]=max_pos_inv(Acl,X)
 
 maxIterations=500;
-Omega0 = X; % initialization
+Omega0 = X.minHRep(); % initialization
 for i = 1:maxIterations
     % compute backward reachable set
     P = Pre_Aut(Acl,Omega0);
     % intersect with the state constraints
     P = P.intersect(Omega0).minHRep();
-    if P==Omega0
+    if P == Omega0
         Oinf=Omega0;
         break
     else
